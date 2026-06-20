51b6c4fcabe8d3b02ad1f6fa433f844436d3cf66
Alonso Machado <84375734+alonso-machado@users.noreply.github.com>
2026-06-20 00:49:21 -0300

    EnvVar Standarization

--- internal/infrastructure/config/config.go
+++ internal/infrastructure/config/config.go
@@ -6,10 +6,10 @@ type Config struct {
 	DatabaseURL       string `envconfig:"DATABASE_URL" required:"true"`
 	RabbitMQURL       string `envconfig:"RABBITMQ_URL" required:"true"`
 	HTTPPort          string `envconfig:"HTTP_PORT" default:"8080"`
-	DispatchInterval  int    `envconfig:"DISPATCH_INTERVAL_MS" default:"500"`
-	DispatchBatchSize int    `envconfig:"DISPATCH_BATCH_SIZE" default:"50"`
-	MaxRetries        int    `envconfig:"MAX_RETRIES" default:"5"`
-	PruneAfterHours   int    `envconfig:"PRUNE_AFTER_HOURS" default:"48"`
+	DispatchInterval  int    `envconfig:"OUTBOX_DISPATCH_INTERVAL_MS" default:"500"`
+	DispatchBatchSize int    `envconfig:"OUTBOX_DISPATCH_BATCH_SIZE" default:"50"`
+	MaxRetries        int    `envconfig:"OUTBOX_MAX_RETRIES" default:"5"`
+	PruneAfterHours   int    `envconfig:"OUTBOX_PRUNE_AFTER_HOURS" default:"48"`
 	PrefetchCount     int    `envconfig:"PREFETCH_COUNT" default:"10"`
 	MaxDeliveries     int    `envconfig:"MAX_DELIVERIES" default:"5"`
 	// PaymentQueue is consumer-worker-only. Not `required` here because Config
